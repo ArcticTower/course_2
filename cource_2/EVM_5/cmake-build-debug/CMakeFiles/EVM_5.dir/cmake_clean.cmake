0fe001155ca36e4ead095c281d7320717baa8b61
ArcticTower <perf@live.ru>
2020-12-17 17:04:04 +0300

    init

---
+++ cource_2/EVM_5/cmake-build-debug/CMakeFiles/EVM_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EVM_5.dir/main.c.o"
+  "EVM_5"
+  "EVM_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EVM_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
